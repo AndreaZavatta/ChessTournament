44cad838a6070690270658ef56f27acce32e9789
Andrea Zavatta <zavattaandrea@gmail.com>
2022-07-04 02:25:29 +0200

    create Allenamento class corresponding to table in db

---
+++ SharedClass/Data/Allenamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Context.Data
+{
+    [Table("Allenamento")]
+    public class Allenamento
+    {
+        [Key]
+        public int Codice { get; set; }
+        public int CodiceAllenatore { get; set; }
+        public int CodiceGiocatore { get; set; }
+
+        [ForeignKey("CodiceAllenatore")]
+        public virtual Allenatore Allenatore { get; set; }
+
+
+    }
+}

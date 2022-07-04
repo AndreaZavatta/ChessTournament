d33643eb976b4f617911a9af24ef2b1ba77d3b64
Andrea Zavatta <zavattaandrea@gmail.com>
2022-07-04 02:25:42 +0200

    create Allenatore class corresponding to table in db

---
+++ SharedClass/Data/Allenatore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Context.Data
+{
+    [Table("Allenatore")]
+    public class Allenatore
+    {
+        [Key]
+        public int Codice { get; set; }
+        public string Federazione { get; set; }
+        public int CodicePersona { get; set; }
+
+    }
+}
